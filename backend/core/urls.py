cd93bbac3f38408f9b7a1cd9311066a7105121a2
Mohd Rameez <mohdrameez@Mohds-MacBook-Air.local>
2026-07-01 22:34:09 +0530

    Health end point

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -1,8 +1,20 @@
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from django.http import JsonResponse
+
+def health(request):
+
+    return JsonResponse({
+
+        "status": "ok",
+
+        "message": "EssenziaT Backend is running"
+
+    })
 
 urlpatterns = [
+    path('',health),
     path('admin/', admin.site.urls),
     path('api/auth/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/auth/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
