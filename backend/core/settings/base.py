15bbdbe8366b55488994092f3a7cb3bc4e3baef2
Mohd Rameez <mohdrameez@Mohds-MacBook-Air.local>
2026-07-03 22:16:21 +0530

    Production Backend Fix

--- backend/core/settings/base.py
+++ backend/core/settings/base.py
@@ -98,6 +98,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 CORS_ALLOW_ALL_ORIGINS = False
 CORS_ALLOWED_ORIGINS = _split_csv(os.getenv('CORS_ALLOWED_ORIGINS', 'http://127.0.0.1:5173,http://localhost:5173'))
 CSRF_TRUSTED_ORIGINS = _split_csv(os.getenv('CSRF_TRUSTED_ORIGINS', 'http://127.0.0.1:5173,http://localhost:5173'))
+CORS_ALLOW_CREDENTIALS = True
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
